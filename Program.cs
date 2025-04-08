54a5df038575fcc19df30396ae9dcc45ec961a70
Mohammad Ilham Firdaus <ilhamfir09@gmail.com>
2025-04-08 07:20:58 +0700

    menambahkan class DataMahasiswa103022300043

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using jurnalmod07;
+
+
+namespace jurnalmod07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa103022300043.ReadJSON();
+            Console.WriteLine();
+            //KuliahMahasiswa_103022300043.ReadJSON();
+        }
+    }
+}
